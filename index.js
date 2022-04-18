53b9548464e28919fb38d92ec3910edc7054cbc1
zisan <uzummzisann@gmail.com>
2022-04-19 00:04:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+const superbowlWin = (array) => {
+  return array.find(findWinner) ? array.find(findWinner).year : undefined;
+};
+
+const findWinner = (object) => object.result === "W";
